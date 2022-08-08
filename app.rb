744bb40ae12e17dff232c9aa2f12a399d29e7b24
Karter Livingston <kdlmuzixnerd14@gmail.com>
2022-08-07 17:38:01 -0700

    commit tree c4481fa34eaad8b40eb69c2980d1107e64e20e22

---
+++ app.rb
@@ -0,0 +1,20 @@
+# -> comments
+
+# puts -> console.log() 
+    # with line break at the end,
+    # makes the data into a string
+puts "Hello World!"
+puts "Pass this test, please."
+# ()-> parenthesis when invoking method is optional
+
+# print -> console.log() no line breaks
+# print "Hello"
+# print "Hello"
+
+# p -> output data in nicer format by calling .inspect
+p [1, 2, 3]
+# equivalent to calling v
+    # puts [1, 2, 3].inspect
+
+# pp -> pretty printing for complex data (nested arrays/hashes)
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
